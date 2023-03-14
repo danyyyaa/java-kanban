d03a2515953347572c822757c1461c2a90bee431
danya <danissim00o@yandex.ru>
2023-03-14 16:38:33 +0700

    refactor: убрать лишнее в KVTaskClient

--- src/managers/server/KVTaskClient.java
+++ src/managers/server/KVTaskClient.java
@@ -1,11 +1,7 @@
 package managers.server;
 
-import com.google.gson.Gson;
-import org.apiguardian.api.API;
-
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,12 +9,10 @@ import java.net.http.HttpResponse;
 
 public class KVTaskClient {
     private final URL url;
-    private Gson gson;
-    private HttpClient client;
-    private String apiToken;
+    private final HttpClient client;
+    private final String apiToken;
 
-    public KVTaskClient(URL url) throws URISyntaxException, IOException, InterruptedException {
-        this.gson = new Gson();
+    public KVTaskClient(URL url) throws IOException, InterruptedException {
         this.url = url;
         client = HttpClient.newHttpClient();
         HttpRequest registrationRequest = HttpRequest.newBuilder()
@@ -26,21 +20,18 @@ public class KVTaskClient {
                 .GET()
                 .build();
 
-        //HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
         HttpResponse<String> response = client.send(registrationRequest, HttpResponse.BodyHandlers.ofString());
         this.apiToken = response.body();
     }
 
     public void put(String key, String json) throws IOException, InterruptedException {
-        URI uriLoad = URI.create(url + "/save/"+ key + "?API_TOKEN=" + apiToken);
+        URI uriSave = URI.create(url + "/save/"+ key + "?API_TOKEN=" + apiToken);
 
         HttpRequest saveRequest = HttpRequest.newBuilder()
-                .uri(uriLoad)
+                .uri(uriSave)
                 .POST(HttpRequest.BodyPublishers.ofString(json))
                 .build();
 
-        //HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
-        //HttpResponse<String> response = client.send(saveRequest, handler);
         client.send(saveRequest, HttpResponse.BodyHandlers.ofString());
     }
 
@@ -52,11 +43,6 @@ public class KVTaskClient {
                 .GET()
                 .build();
 
-        //HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
-        //HttpResponse<String> response = client.send(loadRequest, handler);
-
         return client.send(loadRequest, HttpResponse.BodyHandlers.ofString()).body();
-        //return client.send(loadRequest, handler).body();
-        //return response.body();
     }
 }
