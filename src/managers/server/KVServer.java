56507c487acbfff4d38f0ed5e9952988c5e50861
danya <danissim00o@yandex.ru>
2023-03-14 16:40:21 +0700

    refactor: убрать лишнее в KVServer

--- src/managers/server/KVServer.java
+++ src/managers/server/KVServer.java
@@ -5,18 +5,13 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.sql.SQLOutput;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
-/**
- * Постман: https://www.getpostman.com/collections/a83b61d9e1c81c10575c
- */
 public class KVServer {
     public static final int PORT = 8078;
     private final String apiToken;
